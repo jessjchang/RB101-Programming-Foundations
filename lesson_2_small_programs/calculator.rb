a5f800ff0b0967001dd01c93c5288e7fbb72c120
Jess Chang <jesschang77@gmail.com>
2020-09-22 17:58:55 -0700

    Added bonus features to calculator

--- lesson_2_small_programs/calculator.rb
+++ lesson_2_small_programs/calculator.rb
@@ -1,102 +1,179 @@
-def prompt(message)
+# ---------- YAML module ----------
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# ---------- Constants ----------
+LANGUAGE = {
+  '1' => 'en',
+  '2' => 'fr',
+  '3' => 'es'
+}
+
+# ---------- Methods ----------
+def cleanup
+  Kernel.system('clear')
+end
+
+def messages(message_key, lang='en')
+  MESSAGES[lang][message_key]
+end
+
+def prompt(message_key, lang,
+           custom_data1='', custom_data2='', custom_data3='')
+  if custom_data1
+    message = format(messages(message_key, lang),
+                     custom_data1: custom_data1,
+                     custom_data2: custom_data2,
+                     custom_data3: custom_data3)
+  else
+    message = messages(message_key, lang)
+  end
   Kernel.puts("=> #{message}")
 end
 
+def get_language
+  lang_choice = ''
+  loop do
+    prompt('language_prompt', 'en')
+    lang_choice = Kernel.gets().chomp()
+    break if LANGUAGE.keys.include?(lang_choice)
+    prompt('valid_language', 'en')
+  end
+  LANGUAGE[lang_choice]
+end
+
+def check_name(name)
+  /\A[[:alpha:]]*[[:blank:]]?([[:alpha:]]+)\z/.match(name)
+end
+
+def get_name(lang)
+  name = ''
+  loop do
+    name = Kernel.gets().chomp()
+
+    if check_name(name)
+      break
+    else
+      prompt('valid_name', lang)
+    end
+  end
+  name
+end
+
+def valid_integer?(num)
+  /\A[+-]?\d+\z/.match(num)
+end
+
+def valid_float?(num)
+  /\d/.match(num) && /\A[+-]?\d*\.\d*\z/.match(num)
+end
+
 def valid_number?(num)
-  num.to_i != 0
+  valid_integer?(num) || valid_float?(num)
 end
 
-def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
-end
-
-prompt("Welcome to Calculator! Enter your name:")
-
-name = ''
-loop do
-  name = Kernel.gets().chomp()
-
-  if name.empty?()
-    prompt("Make sure to use a valid name.")
-  else
-    break
-  end
-end
-
-prompt("Hi #{name}!")
-
-loop do # main loop
-  number1 = ''
+def get_number(num_place, lang)
+  number = ''
   loop do
-    prompt("What's the first number?")
-    number1 = Kernel.gets().chomp()
+    prompt(num_place, lang)
+    number = Kernel.gets().chomp()
 
-    if valid_number?(number1)
+    if valid_number?(number)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt('valid_number', lang)
     end
   end
+  number
+end
 
-  number2 = ''
-  loop do
-    prompt("What's the second number?")
-    number2 = Kernel.gets().chomp()
-
-    if valid_number?(number2)
-      break
-    else
-      prompt("Hmm... that doesn't look like a valid number")
-    end
-  end
-
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-  prompt(operator_prompt)
+def get_operator(number2, lang)
+  prompt('operator_prompt', lang)
 
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
 
-    if %w(1 2 3 4).include?(operator)
+    if number2.to_f == 0.0 && operator == '4'
+      prompt('zero_division_message', lang)
+    elsif ['1', '2', '3', '4'].include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3 or 4")
+      prompt('valid_operator', lang)
     end
   end
+  operator
+end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+def operation_to_message(op, lang)
+  message_word = case op
+                 when '1'
+                   messages('adding', lang)
+                 when '2'
+                   messages('subtracting', lang)
+                 when '3'
+                   messages('multiplying', lang)
+                 when '4'
+                   messages('dividing', lang)
+                 end
+  message_word
+end
+
+def final_calc(number1, number2, operator, lang)
+  prompt('operation_message', lang,
+         operation_to_message(operator, lang),
+         number1, number2)
 
   result = case operator
            when '1'
-             number1.to_i() + number2.to_i()
+             number1.to_f() + number2.to_f()
            when '2'
-             number1.to_i() - number2.to_i()
+             number1.to_f() - number2.to_f()
            when '3'
-             number1.to_i() * number2.to_i()
+             number1.to_f() * number2.to_f()
            when '4'
              number1.to_f() / number2.to_f()
            end
 
-  prompt("The result is #{result}")
-
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  prompt('result_message', lang, result)
 end
 
-prompt("Thank you for using the calculator. Good bye!")
+def calc_again(lang)
+  prompt('calculate_again', lang)
+
+  answer = ''
+  loop do
+    answer = Kernel.gets().chomp()
+    break if answer.downcase() == 'y' ||
+             answer.downcase() == 'yes' ||
+             answer.downcase() == 'n' ||
+             answer.downcase() == 'no'
+    prompt('valid_recalculate_input', lang)
+  end
+  answer
+end
+
+# ---------- Program Start ----------
+cleanup()
+
+lang = get_language()
+
+prompt('welcome', lang)
+name = get_name(lang)
+prompt('greeting', lang, name)
+
+# ---------- Main Loop ----------
+loop do
+  number1 = get_number('get_number1', lang)
+  number2 = get_number('get_number2', lang)
+
+  operator = get_operator(number2, lang)
+
+  final_calc(number1, number2, operator, lang)
+
+  answer = calc_again(lang)
+  break if answer.downcase() == 'n' || answer.downcase() == 'no'
+  cleanup()
+end
+
+prompt('goodbye', lang)
